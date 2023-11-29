436d160b0b805a48a1e555e5d1da395a4573c2cc
kim.sg <ksgn1514@gmail.com>
2023-11-29 22:39:12 +0900

    게시글 상세페이지, 부트스트랩 추가

--- main/views.py
+++ main/views.py
@@ -13,3 +13,9 @@ def board(request):
     postlist = Post.objects.all()
     # postlist를 'postlist'라는 이름으로 board.html에 전달
     return render(request, 'main/board.html', {'postlist': postlist})
+
+def posting(request, pk):
+    # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
+    post = Post.objects.get(pk=pk)
+    # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+    return render(request, 'main/posting.html', {'post':post})
