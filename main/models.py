8f8c6987b0499cbb460efbc7e02a24dadcdd72fe
kim.sg <ksgn1514@gmail.com>
2023-11-29 21:48:12 +0900

    board페이지 추가, post모델, 뷰 연동

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# 게시글(Post)엔 제목(title), 내용(content), 게시일(pub_date)이 존재합니다
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    # 게시글의 제목(title)이 Post object 대신하기
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+# View에 Model(Post 게시글) 가져오기
+from .models import Post
 
 # Create your views here.
+# index.html 페이지를 부르는 index 함수
 def index(request):
     return render(request, 'main/index.html')
 
+# board.html 페이지를 부르는 board 함수
 def board(request):
-    return render(request, 'main/board.html')
+    # DB에 있는 모든 Post를 가져와 postlist에 저장
+    postlist = Post.objects.all()
+    # postlist를 'postlist'라는 이름으로 board.html에 전달
+    return render(request, 'main/board.html', {'postlist': postlist})
